5c6befc7d1b0fc709ce9cb79cc3e185c540f8cf9
Devin <frisell@vip.qq.com>
2018-12-27 17:56:35 +0800

    添加uwsgi

--- url.py
+++ url.py
@@ -6,10 +6,10 @@ from application import app
 
 from web.admin.views import blueprint_admin
 from web.home.views import blueprint_home
-from wx import blueprint_wx
+from wechat import wx
 from api.v1 import blueprint_v1
 
 app.register_blueprint(blueprint_admin(), url_prefix="/admin")
 app.register_blueprint(blueprint_home(), url_prefix="/")
-app.register_blueprint(blueprint_wx(), url_prefix="/wx")
+app.register_blueprint(wx, url_prefix="/wx")
 app.register_blueprint(blueprint_v1(), url_prefix="/v1")
